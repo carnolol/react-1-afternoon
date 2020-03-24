3b9d491e51c9b6d1f7bd6087d95cf41c3373a464
Mike Chadwick <mikechadwick@Mikes-MacBook-Pro.local>
2020-03-24 17:18:19 -0600

    step 5

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,45 @@
 import React, {Component} from 'react'
 
 class FilterObject extends Component{
+    constructor(){
+        super()
+        this.state = {
+            pokemon: [
+                {
+                    name: 'Pikachu',
+                    big: false,
+                    color: 'yellow',
+                },
+                 {
+                name: 'Snorlax',
+                big: true,
+                location: 'walkways'
+                 },
+                 {
+                     name: 'Charizard',
+                     big: true,
+                     ability: 'Fire Blast!'
+                 }
+            ],
+            userInput: "",
+            filteredPokemon: []
+        }
+    }
+    handleChange(val){
+        this.setState({userInput: val})
+    }
+    filterPokemon(prop){
+        
+    }
     render(){
         return(
-            <p>Filter Object</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+        <span className="puzzleText"> Original: {JSON.stringify(this.state.pokemon,null ,10)}</span>
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}/>
+                <button className="confirmationButton" onClock={() => this.filterPokemon(this.state.userInput)}>Filter</button>
+        <span className="resultsBox filterObjectRB"> Filtered: {JSON.stringify(this.state.filteredPokemon, null, 10)}</span>
+            </div>
         )
     }
 }
